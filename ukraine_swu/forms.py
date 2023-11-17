a377b38d0d891e07a0c2658cdb5bfba2eaa7cad7
golfimbul19 <golfimbul19@gmail.com>
2023-11-17 14:10:26 +0200

    added login link

--- ukraine_swu/forms.py
+++ ukraine_swu/forms.py
@@ -12,6 +12,6 @@ class TopicForm(forms.ModelForm):
 class EntryForm(forms.ModelForm):
     class Meta:
         model = Entry
-        fields = ['topic', 'header', 'image', 'budget', 'text', 'full_text', 'rich_text']
+        fields = ['topic', 'header', 'image', 'budget', 'text',  'rich_text']
         labels = {'topic':'Категорія проекту','header':'Назва проекту', 'image':'Посилання на головне фото проекту', 'budget': 'Бюджет проекту, грн', 'text': 'Короткий опис проекту',  'rich_text': 'Детальний опис проекту'}
-        widgets = {'text': forms.Textarea(attrs={'cols': 80}), 'image': forms.Textarea(attrs={'cols': 80, 'rows': 1}), 'full_text': forms.Textarea(attrs={'cols': 80}), 'rich_text': CKEditorWidget()}
+        widgets = {'text': forms.Textarea(attrs={'cols': 80}), 'image': forms.Textarea(attrs={'cols': 80, 'rows': 1}), 'rich_text': CKEditorWidget()}
--- ukraine_swu/models.py
+++ ukraine_swu/models.py
@@ -3,23 +3,23 @@ from django.contrib.auth.models import User
 from ckeditor.fields import RichTextField
 
 class Topic(models.Model):
-    """A topic the user is learning about."""
+    """Категорія проектів"""
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        """Return a string representation of the model."""
+        
         return self.text
     
       
 class Entry(models.Model):
-    """Something specific learned about a topic."""
+    """Проекти"""
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     header = models.CharField(max_length=100, default='none')
     text = models.TextField()
     budget = models.IntegerField(default=0)
-    full_text = models.TextField()
+
     rich_text = RichTextField(default="Text")
     image = models.TextField(default='https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png')
     date_added = models.DateTimeField(auto_now_add=True)
@@ -28,9 +28,9 @@ class Entry(models.Model):
         verbose_name_plural = 'entries'
 
     def __str__(self):
-        """Return a simple string representing the entry."""
+       
         return f"{self.text[:50]}..."
-
+"""Волонтери"""
 class Volunteer(models.Model):
     name = models.CharField(max_length=100)
     photo = models.TextField()
