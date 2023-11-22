de02fa13573e4fe8e93aa54e3dca1e9cc09fcef0
golfimbul19 <golfimbul19@gmail.com>
2023-11-22 11:18:22 +0200

    restyled edit_entry and new_entry pages

--- ukraine_swu/forms.py
+++ ukraine_swu/forms.py
@@ -14,4 +14,10 @@ class EntryForm(forms.ModelForm):
         model = Entry
         fields = ['topic', 'header', 'image', 'budget', 'text',  'rich_text']
         labels = {'topic':'Категорія проекту','header':'Назва проекту', 'image':'Посилання на головне фото проекту', 'budget': 'Бюджет проекту, грн', 'text': 'Короткий опис проекту (до 120 символів)',  'rich_text': 'Детальний опис проекту'}
-        widgets = {'text': forms.Textarea(attrs={'cols': 80}), 'image': forms.Textarea(attrs={'cols': 80, 'rows': 1}), 'rich_text': CKEditorWidget()}
+        widgets = {'text': forms.Textarea(attrs={'cols': 120, 'rows': 6, 'placeholder':"Коротко опишіть ваш проект (до 120 символів)", 'style':'resize:none;'}),
+                   'topic':forms.Select(attrs={'cols': 50}),
+                   'header': forms.Textarea(attrs={'cols': 120, 'rows': 3, 'placeholder':"Впишіть назву проекту", 'style':'resize:none;'}), 
+                   'budget': forms.Textarea(attrs={'cols': 60, 'rows': 3, 'placeholder':"Впишіть очікувану вартість", 'style':'resize:none;'}),
+                   'image': forms.Textarea(attrs={'cols': 120, 'rows': 3, 'placeholder':"Вставте посилання на ілюстрацію проекту", 'style':'resize:none;'}), 
+                   'rich_text': CKEditorWidget()
+                   }
